41e69db445f496294f488072883c413c53b3b1dc
Godwin Umosen <umosengodwin568@gmail.com>
2024-01-31 21:11:51 -0800

    director page been settle

--- josepdam/models.py
+++ josepdam/models.py
@@ -9,13 +9,13 @@ class ConstructionPost(models.Model):
     description = models.TextField()
     slug = models.SlugField (max_length=255)
     img = models.ImageField ( upload_to= 'image/')
-    publish_date1 = models.DateTimeField (auto_now_add= True)
+    publish_date = models.DateTimeField (auto_now_add= True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.IntegerField()
     location = models.CharField(max_length = 2200)
 
     class Meta:
-        ordering =['-publish_date1']
+        ordering =['-publish_date']
     
     def __str__(self):
         return self.title + ' | ' + str(self.author)

68be354736f4924f72dfce7cc1349bcc7cd2e165
Godwin Umosen <umosengodwin568@gmail.com>
2024-02-19 08:10:35 -0800

    Images added from the database

--- josepdam/models.py
+++ josepdam/models.py
@@ -13,6 +13,8 @@ class ConstructionPost(models.Model):
     publish_date = models.DateTimeField (auto_now_add= True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.IntegerField()
+    rent = models.IntegerField()
+    Sqm = models.IntegerField()
     location = models.CharField(max_length = 200,blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='blog_posts')
 

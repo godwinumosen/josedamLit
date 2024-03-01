cd26e12fa0808521b29fffdb015edf180750940f
Godwin Umosen <umosengodwin568@gmail.com>
2024-03-01 07:45:17 -0800

    Reloading the session of the portfolio

--- josepdam/views.py
+++ josepdam/views.py
@@ -22,7 +22,7 @@ class HomeView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['second_constructions'] = SecondConstruction.objects.all()
-        return contex
+        return context
 
 
 #The first ArticleDetailView page

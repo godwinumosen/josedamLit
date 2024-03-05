00b04dc62917efeb9ea8aebc94bf09cc5478b6ef
Godwin Umosen <umosengodwin568@gmail.com>
2024-03-05 11:58:55 -0800

    Main logo has been change

--- josepdam/models.py
+++ josepdam/models.py
@@ -103,7 +103,7 @@ class BlogPost(models.Model):
     slug = models.SlugField (max_length=255)
     img = models.ImageField ( upload_to= 'image_blog/')
     publish_date = models.DateTimeField (auto_now_add= True)
-    author = models.ForeignKey(User, on_delete=models.CACADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     location = models.CharField(max_length = 200,blank=True, null=True)
     
     

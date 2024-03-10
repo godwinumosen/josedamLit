406577aa729fd60d1bc6b5b18325de66de18acad
Godwin Umosen <umosengodwin568@gmail.com>
2024-03-10 11:22:05 -0700

    Commit the last like post

--- josepdam/views.py
+++ josepdam/views.py
@@ -25,7 +25,7 @@ class HomeView(ListView):
     
 #The first ArticleDetailView page
 class ArticleDetailView(DetailView):
-    model = ConstructionPost
+    model = ConstructionPos
     template_name = 'josep/article_detail.html'
 
     def ArticleDetailView(request, pk):  
